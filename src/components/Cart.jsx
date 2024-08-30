c6babe7262ea4403d20f0433f7f4e7936ecc862c
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-30 14:57:40 +0300

    Add console log of cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,8 @@
+import { useAppSelector } from "../hooks/hooks";
+
 const Cart = () => {
+  const cartItems = useAppSelector((state) => state.products.cart);
+  console.log("cartItems", cartItems);
   return (
     <div>
       <h1>Cart</h1>
