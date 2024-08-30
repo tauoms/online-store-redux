bff8343271530c94d1cb28c60f46f6d2e9d647a6
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-30 11:02:51 +0300

    Create store folder, store.js, productSlice.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Button } from "react-bootstrap";
+
 function App() {
   return (
     <>
       <div>App will be here</div>
+      <Button style={{ width: "200px" }}>Click me</Button>
+      <Button variant="secondary" size="lg">
+        Click me
+      </Button>
     </>
   );
 }
---
+++ src/store/productSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+    products: [],
+}
+
+export const productSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {},
+
+})
+
+export default productSlice.reducer;
