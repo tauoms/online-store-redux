9d5c854d7f6df6a7abaa57879a304bcf1dc65e09
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-31 17:29:02 +0300

    Add listing of items in cart and total sum on Cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,28 @@
-import { useAppSelector } from "../hooks/hooks";
+import { Button, Container } from "react-bootstrap";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { clearCart } from "../store/productSlice";
 
 const Cart = () => {
   const cartItems = useAppSelector((state) => state.products.cart);
+  const dispatch = useAppDispatch();
   console.log("cartItems", cartItems);
+  const cartSum = cartItems.reduce((accumulator, current) => {
+    return accumulator + current.price;
+  }, 0);
+
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <Container>
+      <h1>Shopping Cart</h1>
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            {item.title} - {item.price} €
+          </li>
+        ))}
+      </ul>
+      <h2>Total: {cartSum.toFixed(2)} €</h2>
+      <Button onClick={() => dispatch(clearCart())}>Empty Cart</Button>
+    </Container>
   );
 };
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,9 +4,9 @@ const Product = ({ product }) => {
   return (
     <Container>
       <Image src={product.image} fluid />
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p>Price: {product.price}</p>
+      <strong>{product.title}</strong>
+      {/* <p>{product.description}</p> */}
+      <p>Price: {product.price} €</p>
       <p>Rating: {product.rating.rate} / 5</p>
     </Container>
   );
